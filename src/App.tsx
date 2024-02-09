a254dc30bacdf3ec6b777970544378ac8e86c9ce
gubriyeeel <officialgabrielsufrir@gmail.com>
2024-02-09 20:07:10 +0800

    chore: improved responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AverageCalculator from "@/components/average-calculator";
 function App() {
   return (
     <main className="h-full w-screen bg-gradient-to-t from-slate-600 to-slate-900 sm:h-screen">
-      <section className="flex flex-col items-center  justify-start w-screen md:justify-center md:flex-row   gap-4 p-4 md:p8 md:items-start md:pt-8">
+      <section className="flex flex-col items-center  justify-start w-screen sm:justify-center sm:flex-row gap-4 p-4 sm:p8 sm:items-start sm:pt-8">
         <GradeCalculator />
         <AverageCalculator />
       </section>
