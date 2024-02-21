c665ab6fad73cdce5952bfe7c342bcc0649ec613
gubriyeeel <officialgabrielsufrir@gmail.com>
2024-02-21 11:10:56 +0800

    style: fixed container height

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Credits from "./components/credits";
 
 function App() {
   return (
-    <main className="flex flex-col justify-between h-full w-screen pb-4 ">
+    <main className="flex flex-col justify-between h-full min-h-screen w-screen pb-4 ">
       <section className="flex flex-col items-center  justify-start w-screen sm:justify-center sm:flex-row gap-4 p-4 sm:p8 sm:items-start sm:pt-8">
         <GradeCalculator />
         <AverageCalculator />
