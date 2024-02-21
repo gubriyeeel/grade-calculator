f328d08f4ee1bcdc5df21e540211d1d77398e6f6
gubriyeeel <officialgabrielsufrir@gmail.com>
2024-02-21 11:09:15 +0800

    style: fixed responsiveness

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
 import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <Toaster />
-  </React.StrictMode>,
-)
+    <div className="h-full bg-gradient-to-t from-slate-600 to-slate-900 pb-8 overflow-x-hidden">
+      <App />
+      <Toaster />
+    </div>
+  </React.StrictMode>
+);
